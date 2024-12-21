2edbf340820911df8bdd5cdc4d2a3467cd9f6db6
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-12-20 22:47:29 -0400

    Making betty proud

--- simple_shell.c
+++ simple_shell.c
@@ -1,42 +1,50 @@
 #include "shell.h"
 /**
  * main - Entry point
+ * @argc: arg count
+ * @argv: arg array
  * Return: Always 0.
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	char *parsed_args[MAXLIST];
-	char *parsed_pipe[MAXLIST];
-	char *str = malloc(1024);
-	int status = 1;
+	char *parsed_args[MAXLIST], *parsed_pipe[MAXLIST], *str = malloc(1024);
+	int status = 1, i;
 	ssize_t bytes_read;
 
-	if (isatty(STDIN_FILENO))
+	if (argc > 1)
 	{
-		while (status)
+		str[0] = '\0';
+		for (i = 1; i < argc; i++)
 		{
-			if (prompt_line(str) == 0)
+			strcat(str, argv[i]);
+			if (i < argc - 1)
+				strcat(str, " ");
+		} status = process_string(str, parsed_args, parsed_pipe);
+		if (status == 1)
+			exe_args(parsed_args);
+		else if (status == 2)
+			exe_args_piped(parsed_args, parsed_pipe);
+	} else
+		if (isatty(STDIN_FILENO))
+		{
+			while (status)
 			{
-				status = process_string(str, parsed_args, parsed_pipe);
-				if (status == 1)
+				if (prompt_line(str) == 0)
 				{
-					exe_args(parsed_args);
-				}
-				else if (status == 2)
-				{
-					exe_args_piped(parsed_args, parsed_pipe);
+					status = process_string(str, parsed_args, parsed_pipe);
+					if (status == 1)
+						exe_args(parsed_args);
+					else if (status == 2)
+						exe_args_piped(parsed_args, parsed_pipe);
 				}
 			}
-		}
-	}
-	else
-	{
-		while ((bytes_read = read(STDIN_FILENO, str, 1024)) > 0)
+		} else
 		{
-			str[bytes_read] = '\0';
-			execute_non_interactive(str);
-		}
-	}
-	free(str);
+			while ((bytes_read = read(STDIN_FILENO, str, 1024)) > 0)
+			{
+				str[bytes_read] = '\0';
+				execute_non_interactive(str);
+			}
+		} free(str);
 	return (0);
 }
