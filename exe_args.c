fc1a6d8f152561290cb9f0e968ae0f3b41d51d74
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-12-21 00:00:13 -0400

    Update prompt line

--- exe_args.c
+++ exe_args.c
@@ -16,6 +16,19 @@ void exe_args(char **parsed)
 	}
 	path_cpy = strdup(path_env);
 	path = strtok(path_cpy, ":");
+	while (path != NULL)
+	{
+		snprintf(full_path, sizeof(full_path), "%s/%s", path, parsed[0]);
+		if (access(full_path, F_OK) == 0)
+			break;
+		path = strtok(NULL, ":");
+	}
+	if (path == NULL)
+	{
+		fprintf(stderr, "%s: command not found\n", parsed[0]);
+		free(path_cpy);
+		return;
+	}
 	pid = fork();
 	if (pid == -1)
 	{
@@ -24,26 +37,13 @@ void exe_args(char **parsed)
 		return;
 	} else if (pid == 0)
 	{
-		while (path != NULL)
-		{
-			snprintf(full_path, sizeof(full_path), "%s/%s", path, parsed[0]);
-			if (access(full_path, F_OK) == 0)
-				break;
-			path = strtok(NULL, ":");
-		}
-		if (path == NULL)
-		{
-			fprintf(stderr, "%s: command not found\n", parsed[0]);
-			free(path_cpy);
-			exit(1);
-		}
 		if (execve(full_path, parsed, NULL) < 0)
 		{
 			perror("Error executing command");
+			free(path_cpy);
 			exit(1);
 		}
-	}
-	else
+	} else
 		wait(NULL);
 	free(path_cpy);
 }
